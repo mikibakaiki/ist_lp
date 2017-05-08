5c1848681f2249b5c23a540dc149b9770a07709c
mikibakaiki <joaomiguelcampos6@gmail.com>
2017-05-08 22:30:08 +0100

    puzzle_muda_propaga() e tira_num() estao a 100%. Adicionei o script ASCII.sh e testes publicos do mooshak.

--- projecto.pl
+++ projecto.pl
@@ -6,11 +6,12 @@
 
 
 :- include('SUDOKU').
+:-['exemplos.pl'].
 
 
 % Base de recursao; Caso em que Cont e o a posicao em questao tem o mesmo valor.
 
-puzzle_muda_propaga(Puz, Pos, Cont, N_Puz) :-
+puzzle_muda_propaga(Puz, Pos, Cont, Puz) :-
     puzzle_ref(Puz, Pos, Cont).
 
 % Se o tamanho de Cont =:= 1, entao, vou propagar.
@@ -20,9 +21,8 @@ puzzle_muda_propaga(Puz, Pos, Cont, N_Puz) :-
 % Por fim, vou verificar se Cont pertence a alguma posicao da lista Pos_Rel.
 % Se sim, entao removo da lista e passo a proxima posicao.
 
-puzzle_muda_propaga(Puz, Pos, [Num], N_Puz) :- !
-
-    puzzle_muda(Puz, Pos, Num, N_Puz),
+puzzle_muda_propaga(Puz, Pos, [Num], New_Puz) :- !,
+    puzzle_muda(Puz, Pos, [Num], N_Puz),
     posicoes_relacionadas(Pos, Pos_Rel),
     tira_num(Num, N_Puz, Pos_Rel, New_Puz).
 
@@ -31,27 +31,12 @@ puzzle_muda_propaga(Puz, Pos, [Num], N_Puz) :- !
 puzzle_muda_propaga(Puz, Pos, Cont, N_Puz) :-
     puzzle_muda(Puz, Pos, Cont, N_Puz).
 
-
-% puzzle_muda_propaga_aux(Puz, [H | T], Cont) :-
-%
-%     puzzle_ref(Puz, H, Cont_Aux),
-%
-%     member(Cont, Cont_Aux),
-%
-%     subtract(Cont_Aux, Cont, /* and what now ??*/),
-%
-%     puzzle_muda_propaga_aux(Puz, T, Cont).
-%
-
 tira_num_aux(Num, Puz, Pos, N_Puz) :-
-
-    % puzzle_ref(Puz, Pos, Cont),
-    % member(Num, Cont),
-    subtract(Pos, Num, Pos)
-    puzzle_ref(Puz, Pos, Cont)
-    puzzle_muda_propaga(Puz, Pos, Cont, N_Puz).
+    puzzle_ref(Puz, Pos, Cont),
+    subtract(Cont, [Num], Cont1),
+    puzzle_muda_propaga(Puz, Pos, Cont1, N_Puz).
 
 tira_num(Num, Puz, Posicoes, N_Puz) :-
-    tira_num_aux(Num, Puz, Pos, N_Puz).
+    percorre_muda_Puz(Puz, tira_num_aux(Num), Posicoes, N_Puz).
 
 %
